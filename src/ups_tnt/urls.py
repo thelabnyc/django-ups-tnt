b8673578ca775fad5032bc46cdb0db9fb8d80d85
Craig Weber <crgwbr@gmail.com>
2024-06-26 11:22:28 -0400

    Refs #26187 Migrate re_path to path

--- src/ups_tnt/urls.py
+++ src/ups_tnt/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import TimeInTransitView
 
 
 urlpatterns = [
-    re_path(r"^tnt/$", TimeInTransitView.as_view(), name="time-in-transit"),
+    path("tnt/", TimeInTransitView.as_view(), name="time-in-transit"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
